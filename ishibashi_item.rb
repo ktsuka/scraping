b0480f874e297d1182c2cd32127fb2b92a5bc759
ktsuka <s3000xl16mb@yahoo.co.jp>
2018-09-24 22:01:22 +0900

    update 20180924

--- ishibashi_item.rb
+++ ishibashi_item.rb
@@ -1,38 +1,52 @@
 # -*- coding: utf-8 -*-
 require 'nokogiri'
 require 'open-uri'
+$stdout.sync = true
 
+# progressive bar function
+def progress_bar(i, max)
+  i = max if i > max
+  rest_size = 1 + 5 + 1      # space + progress_num + %
+  bar_width = 79 - rest_size # (width - 1) - rest_size = 72
+  percent = i * 100.0 / max
+  bar_length = i * bar_width.to_f / max
+  bar_str = ('#' * bar_length).ljust(bar_width)
+  progress_num = '%3.1f' % percent
+  print "\r#{bar_str} #{'%5s' % progress_num}%"
+end
+
+# Array
 item_array = Array.new
 price_array = Array.new
 
+# ISHIBASHI STORE DIGITAL URL
 prev_url = "https://store.ishibashi.co.jp/ec/proList/doSearch/srDispProductList/1/1/%20/11430000000/1/%20/%20/%20/%20/%20/%20/1/40/"
 post_url = "/0?jp=on&wd=%20&searchType=0&excludeSearchWord=&facetSearchType=0"
 item_num = 0
 total_num = 0
 
+# get item all counts
 url = prev_url + item_num.to_s + post_url
 doc = Nokogiri::HTML(open(url))
-
-# get item all counts
 doc.xpath("//span[@class=\"color\"]").each do |count|
     total_num =  count.text.to_i
     break
 end
 
-puts total_num
+#puts total_num
 
 # 
 while item_num < total_num
 
     # set url
     url = prev_url + item_num.to_s + post_url
-    puts url
+    #puts url
 
     # get item list
     doc = Nokogiri::HTML(open(url))
     doc.xpath("//p[@class=\"item\"]").each do |item|
         #puts item.text.gsub("\/ ","").gsub(" …","").gsub(" ","")
-        item_array.push(item.text.gsub("\/ ","").gsub(" …","").gsub(" ",""))
+        item_array.push(item.text.gsub("\/ ","").gsub(" …",""))
     end
 
     # get price list
@@ -42,15 +56,50 @@ while item_num < total_num
     end
 
     item_num += 40
+    progress_bar(item_num,total_num)
     sleep(1)
+    break
 
 end
 
 (0...item_array.length).map{|i| [item_array[i], price_array[i]]}
 item_list = item_array.zip(price_array)
 
+#i = 0
+#while i < item_list.length do
+#    puts (i + 1).to_s + "," + item_list[i][0] + "," + item_list[i][1]
+#    i += 1
+#end
+
+# put results into file
 i = 0
-while i < item_list.length do
-    puts (i + 1).to_s + "," + item_list[i][0] + "," + item_list[i][1]
-    i += 1
+datetime = Time.new
+filename = "\/Users\/tsuka\/ruby\/scraping\/ishibashi_digital_" + datetime.strftime("%Y%m%d%H%M") + ".txt"
+File.open(filename,"w") do |f|
+    while i < item_list.length do
+        #puts item_list[i][0]
+        info =  item_list[i][0].split(" ")
+        #if item_list[i][0] =~ /【(\W+)】*/ || item_list[i][0] =~ /【(\W+\d+)】*/ then
+        #if item_list[i][0] =~ /【(\W+)】*/ then
+        #    puts item_list[i][0]
+        #    puts $1
+        #end
+        itype = ""
+        #if item_list[i][0] =~ /[特価]/ then
+        #   itype = "special"
+        #end
+        #if item_list[i][0] =~ /[中古]/ then
+        #   itype = "old"
+        #end
+        if info[0] == "Dave" then
+            puts datetime.strftime("%Y%m%d") + sprintf("%05d",i + 1).to_s + "," + info[0..2].join(" ") + "," + info[3..-1].join(" ") + "," + item_list[i][1] \
+            + "," + "" + "," + itype
+        else
+            puts datetime.strftime("%Y%m%d") + sprintf("%05d",i + 1).to_s + "," + info[0] + "," + info[1..-1].join(" ") + "," + item_list[i][1] \
+            + "," + "" + "," + itype
+        end
+        i += 1
+    end
 end
+
+puts "SCRAPING DONE!"
